1fc0196f04b8b8ee83a3a4523812e851f0f44e3d
Muhammadumairtanveer <umair799284@gmail.com>
2023-09-16 21:54:58 +0500

    update feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,183 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 function App() {
-  
-  const [task, settask] = useState("")
-  const [date, setdate] = useState("")
-  const [time, settime] = useState("")
-  const [updatebtn, setupdatebtn] = useState(false)
-  const [mainTask, setmainTask] = useState([])
+  const [task, setTask] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [updateBtn, setUpdateBtn] = useState(false);
+  const [mainTask, setMainTask] = useState([]);
+  const [updateIndex, setUpdateIndex] = useState(null);
 
-  const addHandler = (e)=>{
-    e.preventDefault()
-    setmainTask([...mainTask, {task,date,time}])
-    settask("")
-    setdate("")
-    settime("")
+  const addHandler = (e) => {
+    e.preventDefault();
+    if (updateBtn) {
+      // If update mode is active, update the task at the specified index
+      const updatedTasks = [...mainTask];
+      updatedTasks[updateIndex] = { task, date, time };
+      setMainTask(updatedTasks);
+      setTask('');
+      setDate('');
+      setTime('');
+      setUpdateBtn(false);
+      setUpdateIndex(null);
+    } else {
+      // Otherwise, add a new task
 
-  }
+      setMainTask([...mainTask, { task, date, time }]);
+      setTask('');
+      setDate('');
+      setTime('');
+    }
+  };
 
-  const del = (i)=>{
-    const data = mainTask.filter((item,index)=> i !=index)
-    console.log(data)
-    setmainTask([...data])
-  }
- const update = (i)=>{
-  const data = mainTask.filter((item,index)=> i == index)
-  data.map((i)=>{
-    settask(i.task)
-    setdate(i.date)
-    settime(i.time)
-  })
-  setupdatebtn(true)
- }
+  const del = (i) => {
+    const data = mainTask.filter((item, index) => i !== index);
+    setMainTask([...data]);
+  };
+
+  const update = (i) => {
+    const data = mainTask.filter((item, index) => i === index);
+    data.map((item) => {
+      setTask(item.task);
+      setDate(item.date);
+      setTime(item.time);
+    });
+    setUpdateBtn(true);
+    setUpdateIndex(i);
+  };
 
   return (
     <div className='container mt-5'>
       <form>
-     <div class="mb-3">
-    <label for="text" class="form-label">Enter Your Task</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={task} onChange={(e)=>{
-       settask(e.target.value)
-    }} />
-    </div>
-    <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Enter Your Task</label>
-    <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={date} onChange={(e)=>{
-       setdate(e.target.value)
-    }} />
-    </div>
-    <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Enter Your Task</label>
-    <input type="time" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={time} onChange={(e)=>{
-      settime(e.target.value)
-    }} />
-    </div>
-    {/* <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> */}
-  
-  {/* <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" />
-  </div> */}
-  {/* <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div> */}
-  {updatebtn ? <button style={{borderRadius:"6px", backgroundColor:"green", color:"white", border:"none"}}>update</button> :<button onClick={addHandler} type="submit" class="btn btn-primary">Add Task</button>}
-</form>
-<hr />
-<div className='container'>
-  <div className='d-flex justify-content-around'>
-    <h3>Task</h3>
-    <h3>Date</h3>
-    <h3>Time</h3>
+        <div className='mb-3'>
+          <label htmlFor='task' className='form-label'>
+            Enter Your Task
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            id='task'
+            aria-describedby='emailHelp'
+            value={task}
+            onChange={(e) => {
+              setTask(e.target.value);
+            }}
+          />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='date' className='form-label'>
+            Enter Date
+          </label>
+          <input
+            type='date'
+            className='form-control'
+            id='date'
+            aria-describedby='emailHelp'
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='time' className='form-label'>
+            Enter Time
+          </label>
+          <input
+            type='time'
+            className='form-control'
+            id='time'
+            aria-describedby='emailHelp'
+            value={time}
+            onChange={(e) => {
+              setTime(e.target.value);
+            }}
+          />
+        </div>
 
-    <h3>Action</h3>
-  </div>
- {mainTask.map((t,i)=>{
-  return <div className='d-flex justify-content-around'>
-    <h5>{t.task}</h5>
-    <h5>{t.date}</h5>
-    <h5>{t.time}</h5>
-    <div className='d-flex justify-content-around' style={{width:"155px"}}>
-    <button onClick={()=>del(i)} style={{borderRadius:"6px", backgroundColor:"red", color:"white", border:"none"}} >Delete</button>
-    <button onClick={()=>update(i)} style={{borderRadius:"6px", backgroundColor:"green", color:"white", border:"none"}} >Uptate</button>
+        {updateBtn ? (
+          <button
+            onClick={addHandler}
+            style={{
+              borderRadius: '6px',
+              backgroundColor: 'green',
+              color: 'white',
+              border: 'none',
+            }}
+          >
+            Update
+          </button>
+        ) : (
+          <button
+            onClick={addHandler}
+            type='submit'
+            className='btn btn-primary'
+          >
+            Add Task
+          </button>
+        )}
+      </form>
+      <hr />
+      <div className='container'>
+       
+        <table class="table">
+                  <thead>
+                    <tr>
+                    <th scope="col">Task no</th>
+                      <th scope="col">Task</th>
+                      <th scope="col">Date</th>
+                      <th scope="col">Time</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                </thead>
+        {mainTask.map((t, i) => {
+          return (
+          
+          <tbody className='mb-4' key={i}>
+            <tr>
+              <th scope="row">1</th>
+              <td>{t.task}</td>
+              <td>{t.date}</td>
+              <td>{t.time}</td>
+              <td>
+              <button
+                 className='btn btn-outline-danger me-4 btnmb'
+                  onClick={() => del(i)}
+                  style={{
+                    borderRadius: '6px',
+                    backgroundColor: 'red',
+                    color: 'white',
+                    border: 'none',
+                  }}
+                >
+                  Delete
+              </button>
+              <button
+                  className='btn btn-outline-success'
+                  onClick={() => update(i)}
+                  style={{
+                    borderRadius: '6px',
+                    backgroundColor: 'green',
+                    color: 'white',
+                    border: 'none',
+                  }}
+                >
+                  Update
+                </button>
+                
+              </td>
+            </tr>
+    
+          
+          </tbody>
+         
+          
+          );
+        })}
+      </table> 
     </div>
-  </div>
- })
-
- } 
- 
-</div>
-</div>
-  )
+    </div>
+  );
 }
 
-
-export default App
+export default App;
